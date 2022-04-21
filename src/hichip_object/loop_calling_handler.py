fb5040f569ed4b1f387d6a3ff444a9ebb57c6d12
hanbinlu <lhb032@gmail.com>
2022-04-20 20:26:52 -0700

    p2np

--- src/hichip_object/loop_calling_handler.py
+++ src/hichip_object/loop_calling_handler.py
@@ -6,7 +6,7 @@ from statsmodels.stats.multitest import multipletests
 from .load_loop_data import (
     loop_PET_count,
     putative_loops_from_anchors,
-    putative_p2a_loops,
+    putative_p2np_loops,
     anchor_depth_by_anchor_PETs,
     anchor_depth_by_A2N_PETs,
 )
@@ -121,7 +121,7 @@ class GenericLoopCallHandler(object):
         )
 
     @classmethod
-    def P2A_Loops(
+    def P2NP_Loops(
         self,
         vp,
         genomic_bins,
@@ -129,12 +129,6 @@ class GenericLoopCallHandler(object):
         parallel=20,
     ):
         """Call significant loops from P2A bins"""
-
-        # Notes:
-        # 1. putative_loops will be connectome of all bins within distance range
-        #   For 2.5 kb resolution in mouse genome, it will be a list of # loops
-        # 2.
-
         # genomic_bins are pd.DataFrame in bed format and are sorted
         # colnames ["chro", "start", "end"] (add check for colname obligation)
         # 4th column "Num_Anchors", non zero value indicates the correpondent gb has anchors
@@ -149,7 +143,7 @@ class GenericLoopCallHandler(object):
             parallel,
         )
         # intra, dist qualified anchor pairs
-        putative_loops = putative_p2a_loops(
+        putative_loops = putative_p2np_loops(
             self.genomic_bins, inter_range, parallel
         )
 
@@ -167,8 +161,85 @@ class GenericLoopCallHandler(object):
             + self.contact_matrix.sum(axis=1).A1
         )
 
-        sig_mean = gb_anchor_depth[gb_anchor_depth != 0].mean()
-        gb_anchor_depth /= sig_mean
+        # anchor and non-anchor signal mean
+        # anchor_bin depth is the sum of interaction with non anchor bin
+        # non anchor bin depth is the sum of anchor bin
+        is_anchor = (genomic_bins.Num_Anchors != 0).values
+        gb_anchor_depth[is_anchor] /= gb_anchor_depth[is_anchor].mean()
+        gb_anchor_depth[~is_anchor] /= gb_anchor_depth[~is_anchor].mean()
+
+        # mask 0 depth anchors
+        mask_opt = diags(gb_anchor_depth != 0, format="csc", dtype=int)
+        putative_loops = mask_opt * putative_loops * mask_opt
+        loop_pet = mask_opt * loop_pet * mask_opt
+
+        # organize loop_pet and putative_loops into metric table
+        # loop_metric is data for all putative loops
+        logging.info("Joining data")
+        self.loop_metric = _compile_loop_metric(
+            loop_pet, putative_loops, gb_anchor_depth, is_anchor
+        )
+        self.interaction_statistics = self.loop_metric.loc[
+            self.loop_metric.C != 0
+        ].copy()
+
+        self.genomic_bins["Depth"] = gb_anchor_depth
+        self.count_cut = self.loop_metric.C[self.loop_metric.C != 0].mean()
+
+        logging.info(
+            f"{sum(self.genomic_bins.Num_Anchors != 0)} of anchors forms {len(self.loop_metric)} putative mid-range P2A loops"
+        )
+        logging.info(
+            f"{len(self.loop_metric[self.loop_metric.C != 0])} observed loops that contains {self.loop_metric.C.sum()} PETs (avg = {self.count_cut})"
+        )
+
+    @classmethod
+    def A2A_Loops(
+        self,
+        vp,
+        genomic_bins,
+        inter_range=(5000, 2_000_000),
+        parallel=20,
+    ):
+        """Call significant loops from A2A bins"""
+        # genomic_bins are pd.DataFrame in bed format and are sorted
+        # colnames ["chro", "start", "end"] (add check for colname obligation)
+        # 4th column "Num_Anchors", non zero value indicates the correpondent gb has anchors
+        if any(genomic_bins.index.values != np.arange(len(genomic_bins))):
+            raise ValueError("Anchors index must be RangeIndex")
+        self.genomic_bins = genomic_bins.copy()
+
+        # interaction distance to be considered
+        self.dist_low, self.dist_high, self.parallel = (
+            inter_range[0],
+            inter_range[1],
+            parallel,
+        )
+        # intra, dist qualified anchor pairs
+        fake_anchors = genomic_bins.copy()
+        fake_anchors["Num_Anchors"] += 1
+        putative_loops = putative_loops_from_anchors(
+            fake_anchors, inter_range, parallel
+        )
+
+        # read interaction data into anchor-anchor sparse matrix
+        logging.info("Loading loop PETs")
+        # loading hic matrix
+        self.contact_matrix = build_hic_matrix(
+            vp, self.genomic_bins, self.parallel
+        )
+        # drop loop_pets of unqualified anchor pairs
+        loop_pet = self.contact_matrix.multiply(putative_loops != 0)
+        # count bin depth by all vp
+        gb_anchor_depth = (
+            self.contact_matrix.sum(axis=0).A1
+            + self.contact_matrix.sum(axis=1).A1
+        )
+
+        # anchor and non-anchor signal mean
+        # anchor_bin depth is the sum of interaction with non anchor bin
+        # non anchor bin depth is the sum of anchor bin
+        gb_anchor_depth /= gb_anchor_depth[gb_anchor_depth != 0].mean()
 
         # mask 0 depth anchors
         mask_opt = diags(gb_anchor_depth != 0, format="csc", dtype=int)
@@ -253,30 +324,69 @@ class GenericLoopCallHandler(object):
         )
 
 
-def _compile_loop_metric(loop_pet, putative_loops, anchor_depth):
+def _compile_loop_metric(
+    loop_pet, putative_loops, anchor_depth, is_anchor=None
+):
     """
     Integrate sparse matrix data `loop_pet`, `dist_anchor_pairs`, `product_depth` into one DataFrame
     """
     putative_loops_data = putative_loops.tocoo()
 
     # group the data together. np.array([I, J, counts, features])
-    df = pd.DataFrame(
-        {
-            "I": putative_loops_data.row,
-            "J": putative_loops_data.col,
-            "L": putative_loops_data.data,
-            "D_i": anchor_depth[putative_loops_data.row],
-            "D_j": anchor_depth[putative_loops_data.col],
-            "D": anchor_depth[putative_loops_data.row]
-            * anchor_depth[putative_loops_data.col],
-            # add count values; only dist qualified anchor-anchor pairs
-            "C": [
-                loop_pet[i, j]
-                for i, j in zip(
-                    putative_loops_data.row, putative_loops_data.col
-                )
-            ],
-        }
-    ).sort_values(by=["L", "C"], ignore_index=True)
+    if is_anchor is None:
+        df = pd.DataFrame(
+            {
+                "I": putative_loops_data.row,
+                "J": putative_loops_data.col,
+                "L": putative_loops_data.data,
+                "D_i": anchor_depth[putative_loops_data.row],
+                "D_j": anchor_depth[putative_loops_data.col],
+                "D": anchor_depth[putative_loops_data.row]
+                * anchor_depth[putative_loops_data.col],
+                # add count values; only dist qualified anchor-anchor pairs
+                "C": [
+                    loop_pet[i, j]
+                    for i, j in zip(
+                        putative_loops_data.row, putative_loops_data.col
+                    )
+                ],
+            }
+        ).sort_values(by=["L", "C"], ignore_index=True)
+    else:
+        # D_i as anchor, D_j non_anchor, no product depth
+        # only use for p2np mode
+        is_anchor_i, is_anchor_j = (
+            is_anchor[putative_loops_data.row],
+            is_anchor[putative_loops_data.col],
+        )
+        if not all(np.logical_xor(is_anchor_i, is_anchor_j)):
+            raise ValueError("P2NP mode only")
+
+        I = (
+            putative_loops_data.row * is_anchor_i
+            + putative_loops_data.col * is_anchor_j
+        )
+        J = putative_loops_data.row * np.logical_not(
+            is_anchor_i
+        ) + putative_loops_data.col * np.logical_not(is_anchor_j)
+
+        df = pd.DataFrame(
+            {
+                "I": I,
+                "J": J,
+                "L": putative_loops_data.data,
+                "D_i": anchor_depth[I],
+                "D_j": anchor_depth[J],
+                "D": anchor_depth[putative_loops_data.row]
+                * anchor_depth[putative_loops_data.col],
+                # add count values; only dist qualified anchor-anchor pairs
+                "C": [
+                    loop_pet[i, j]
+                    for i, j in zip(
+                        putative_loops_data.row, putative_loops_data.col
+                    )
+                ],
+            }
+        ).sort_values(by=["L", "C"], ignore_index=True)
 
     return df
